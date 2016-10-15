17149b576047f8a3c2895bb7ed7bac8d2635a9db
Houfeng <admin@xhou.net>
2016-10-15 15:48:02 +0800

    1.0.1

--- README.md
+++ README.md
@@ -7,9 +7,28 @@ npm install oneport --save
 ```
 
 # 使用
+
 ```js
-oneport(function(err,port){
-  if(err) return console.error(err);
+
+
+//获取一个空闲的端口
+oneport.acquire(function (err, port) {
+  if (err) return console.error(err);
   console.log('port:', port);
 });
+
+let guid = '3aac50f1-bdb2-44c0-3eb0-228aa48bbf3a';
+
+//获取一个端口，并指定一个「名称」
+oneport.acquire(guid, function (err, port) {
+  if (err) return console.error(err);
+  console.log('port:', port);
+});
+
+//通过名称查询最后一次成功获取的端口
+oneport.last(guid, function (err, port) {
+  if (err) return console.error(err);
+  console.log('port:', port);
+});
+
 ```
--- test/test.js
+++ test/test.js
@@ -1,7 +1,27 @@
 const oneport = require('../');
+const utils = require('ntils');
+const assert = require('assert');
 
-console.time('time');
-oneport(function (err, port) {
-  console.log(port);
-  console.timeEnd('time');
+describe('oneport', function () {
+
+  it('save', function (done) {
+    oneport.acquire(function (err, port) {
+      if (err) throw err;
+      assert.strictEqual(utils.isNumber(port), true);
+      done();
+    });
+  });
+
+  it('no save', function (done) {
+    oneport.acquire('test', function (err, port1) {
+      if (err) throw err;
+      assert.strictEqual(utils.isNumber(port1), true);
+      oneport.last('test', function (err, port2) {
+        if (err) throw err;
+        assert.strictEqual(port1, port2);
+        done();
+      });
+    });
+  });
+
 });
